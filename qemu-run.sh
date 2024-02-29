e621d17644678e43469aa045f650cc0ed6d96597
Bill Mills <bill.mills@linaro.org>
2024-02-28 20:41:38 -0500

    qemu-run.sh: Prep for TRS mode
    Change some base options to be more like TRS LAVA jobs:
    * cortex-a57 not a53
    * get rid of file backed shared memory model
    * use -nographic instead of -display none (should be the same)
    
    Move things from base to case specific in prep for trs case:
    * -machine so we can control secure=on or not
    * virtio type and file for hard disk image
    * what firmware we are using and how
    
    Switch to our local copy of QEMU firmware:
    * use tfa/optee/u-boot
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

--- qemu-run.sh
+++ qemu-run.sh
@@ -3,45 +3,58 @@
 MY_DIR=$PWD
 
 QEMU_BASE="-machine type=virt \
-           -machine type=virt,virtualization=on \
-           -cpu cortex-a53 \
+           -cpu cortex-a57 \
            -smp 8 \
            -accel tcg \
            -device virtio-net-pci,netdev=unet \
-           -device virtio-scsi-pci \
-           -device scsi-hd,drive=hd \
            -netdev user,id=unet,hostfwd=tcp::2222-:22 \
-           -blockdev driver=raw,node-name=hd,file.driver=file,file.filename=${MY_DIR}/generated/debian-12-arm64.img \
            -serial mon:stdio \
            -m 8192 \
-           -object memory-backend-memfd,id=mem,size=8G,share=on \
-           -display none"
+           -nographic"
+
+# U-boot can't handle virtio-scsi ATM so use virtio-blk (and /dev/vda)
+# but for now leave the choice to the specific case
+: \
+           -device virtio-scsi-pci \
+           -device scsi-hd,drive=hd \
+
 
 case $1 in
+
 ""|"uefi"|"efi")
     qemu-system-aarch64 $QEMU_BASE \
-           -bios /usr/share/qemu-efi-aarch64/QEMU_EFI.fd
+        -machine type=virt,virtualization=on,secure=on \
+        -device virtio-blk-device,drive=hd \
+        -blockdev driver=raw,node-name=hd,file.driver=file,file.filename=${MY_DIR}/generated/debian-12-arm64.img \
+        -bios ${MY_DIR}/qemu-firmware/arm64-tfa-optee-uboot.bin
     ;;
 "direct"|"linux"|"linux-direct")
     qemu-system-aarch64 $QEMU_BASE \
+        -machine type=virt,virtualization=on \
+        -device virtio-blk-device,drive=hd \
+        -blockdev driver=raw,node-name=hd,file.driver=file,file.filename=${MY_DIR}/generated/debian-12-arm64.img \
         -kernel ${MY_DIR}/direct-boot/Image \
         -initrd ${MY_DIR}/direct-boot/initrd.img \
-        -append "console=ttyAMA0 root=/dev/sda2"
-
+        -append "console=ttyAMA0 root=/dev/vda2"
     ;;
+
 "xen-direct"|"xen")
     qemu-system-aarch64 $QEMU_BASE \
+        -machine type=virt,virtualization=on \
+        -blockdev driver=raw,node-name=hd,file.driver=file,file.filename=${MY_DIR}/generated/debian-12-arm64.img \
         -kernel ${MY_DIR}/direct-boot/xen.efi \
-        -append dom0_mem=4G,max:4G -append loglvl=all -append guest_loglvl=all \
+        -append "dom0_mem=4G,max:4G loglvl=all guest_loglvl=all" \
         -device guest-loader,\
 addr=0x49000000,kernel=${MY_DIR}/direct-boot/Image,\
-bootargs="console=hvc0 earlyprintk=xen root=/dev/sda2" \
+bootargs="console=hvc0 earlyprintk=xen root=/dev/vda2" \
         -device guest-loader,\
 addr=0x50000000,initrd=${MY_DIR}/direct-boot/initrd.img
     ;;
+
 *)
     echo "unknown option $1"
     ;;
+
 esac
 
 
