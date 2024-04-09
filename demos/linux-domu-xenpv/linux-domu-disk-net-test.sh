75135dc6257416fffea9ec8ab7c713bdd6c883b1
Bill Mills <bill.mills@linaro.org>
2024-04-08 23:42:29 -0400

    demos: update linux demos to 6.6.23 version of min
    Also
    * we don't need to gic version as Linux will work with either.
    * don't preserve by on crash, let it terminate
    * we can enable the nic now that we have xenbr0
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

---
+++ demos/linux-domu-xenpv/linux-domu-disk-net-test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+run_test() {
+    dd if=/dev/zero of=disk0.img bs=1M count=16
+    xl vcpu-list
+    xl create ./linux-domu.conf 'name="test1"'
+    xl vcpu-list
+    xl block-list test1
+    xl network-list test1
+    sleep 20
+    xl destroy test1
+    xl vcpu-list
+}
+
+case $1 in
+""|"run")
+    shift
+    run_test "$@"
+    ;;
+"files")
+    echo linux-domu-disk-net.conf $IMAGES/min
+    ;;
+*)
+    echo "$0: unknown command $1"
+    false
+    ;;
+esac
