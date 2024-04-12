d7817c8c6bcf075b0424106c05f11d9f781daecb
Bill Mills <bill.mills@linaro.org>
2024-04-12 16:16:19 -0400

    tests: auto test for min-tftp (Broken)
    This is where I was on doing autotest of min-tftp.
    However min does not have bash so my existing technique won't work.
    I don't have time to fix test-qemu now but check-in this start.
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

---
+++ tests/min-tftp/dummy-test.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+MODE=pass
+
+run_test() {
+    echo "This is the dummy test: $@"
+    case $MODE in
+    "pass"|"")
+        sleep 5
+        true
+        ;;
+    "fail")
+        sleep 5
+        false
+        ;;
+    "timeout")
+        sleep 600
+        ;;
+    "hang")
+        systemctl stop sshd
+        ;;
+    esac
+}
+
+case $1 in
+"run")
+    shift
+    run_test "$@"
+    ;;
+"files")
+    echo "dummy-file $IMAGES/zephyr-apps"
+    ;;
+*)
+    echo "unknown command $1"
+    false
+    ;;
+esac
