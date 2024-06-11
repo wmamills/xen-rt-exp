a303cb0ab80b217853f4fec85e6bcb0698d5531c
Bill Mills <bill.mills@linaro.org>
2024-06-11 08:26:23 -0400

    qemu: add xilinx and openamp variants, bump to official v9.0.0
    in build-qemu.sh:
    * Add builds for xilinx and openamp variants
    * Upstream build is now official v9.0.0 instead of -rc1
    * Xilinx variant builds from Xilinx fork and uses v2024.1 version
      * Xilinx variant will need libpixmap as no disable for 8.x baseline
      * Xilinx variant builds microblaze (little endian) also
    * Openamp is just a copy of v9.0.0 for now so no images
      * Will be staged versions like ivshmem-flat support etc
    * Explicitly disable graphics and sounds etc so results are consistent
      even if those libraries are present
    * Install libslirp so we get an ABI version compatible with our build
      machine (4.1 in ubuntu 20.04) instead of assuming a default of 4.7
      which does not work on ubuntu 20.04 or 22.04.
    * Add ssh and ec2 build methods
    * for all remote build methods put the result in the correct spot
      in saved-images.
    
    In qemu-run:
    * adjust for new name
    
    In saved-images:
    * record new versions for upstream and xilinx variants
    * skip openamp for now as it is not different than upstream
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

--- image-build/build-qemu.sh
+++ image-build/build-qemu.sh
@@ -5,12 +5,48 @@ MY_NAME=$(basename $ME)
 
 JOB_NAME=qemu-build-$(date +%Y-%m-%d-%H%M%S)
 
-NAME=qemu-9-wip
-VER=v9.0.0-rc1
+for i in "$@"; do
+    VAL=${i#*=}
+    case $i in
+    VARIANT=*)
+        VARIANT=$VAL
+        ;;
+    esac
+done
+
+TARGET_LIST="aarch64-softmmu"
+EXTRA_CONFIG="--disable-pixman"
+
+case $VARIANT in
+""|"upstream")
+    NAME=qemu-v9
+    VER=v9.0.0
+    URL=https://gitlab.com/qemu-project/qemu.git
+    ;;
+"xilinx")
+    NAME=qemu-xilinx-v2024.1
+    VER=xilinx_v2024.1
+    URL=https://github.com/Xilinx/qemu.git
+    TARGET_LIST="aarch64-softmmu,microblazeel-softmmu"
+    EXTRA_CONFIG=""
+    ;;
+"openamp")
+    NAME=qemu-openamp-v2024.05
+    VER=origin/v2024.05
+    URL=https://github.com/OpenAMP/qemu-openamp-staging.git
+    ;;
+*)
+    echo "unknown variant $2"
+    exit 2
+    ;;
+esac
+
+echo "VARIANT=$VARIANT"
 
 admin_setup() {
-    apt update
-    apt install -y git build-essential python3-pip python3-venv ninja-build libglib2.0-dev libncurses-dev
+    apt-get update
+    apt-get install -y git build-essential python3-pip python3-venv ninja-build \
+        libglib2.0-dev libncurses-dev libpixman-1-dev libslirp-dev
     rm -rf /opt/$NAME
     mkdir /opt/$NAME
     echo chown $1 /opt/$NAME
@@ -20,19 +56,31 @@ admin_setup() {
 prj_build() {
     ORIG_PWD=$PWD
 
-    git clone https://gitlab.com/qemu-project/qemu.git
+    git clone $URL qemu
     cd qemu
     git reset --hard $VER
 
     mkdir ../build; cd ../build
 
-    ../qemu/configure --target-list="aarch64-softmmu" --enable-fdt --enable-slirp --disable-docs --disable-oss --enable-strip --prefix=/opt/$NAME
+    ../qemu/configure --target-list="$TARGET_LIST" --prefix=/opt/$NAME \
+        --enable-fdt --enable-slirp --enable-strip \
+        --disable-docs \
+        --disable-gtk --disable-opengl --disable-sdl \
+        --disable-dbus-display --disable-virglrenderer \
+        --disable-vte --disable-brlapi \
+        --disable-alsa --disable-jack --disable-oss --disable-pa \
+        $EXTRA_CONFIG
+
     make -j10
     make install
     for i in ivshmem-{client,server}; do
         cp contrib/$i/$i /opt/$NAME/bin/$i;
     done
-    find /opt/$NAME/share/qemu/ -type f ! -name "efi-*" ! -name "edk2-aarch64*" ! -name "edk2-arm-vars*" ! -name "edk2-*.txt" ! -name "*-edk2-*" ! -name "en-us" | xargs rm
+
+    # trim the fat, get rid of the x86 specific roms and extra languages etc
+    find /opt/$NAME/share/qemu/ -type f \
+        ! -name "efi-*" ! -name "edk2-aarch64*" ! -name "edk2-arm-vars*" \
+        ! -name "edk2-*.txt" ! -name "*-edk2-*" ! -name "en-us" | xargs rm
 
     cd $ORIG_PWD
     tar cvzf $NAME.tar.gz -C /opt $NAME
@@ -44,16 +92,44 @@ case $1 in
     admin_setup "$@"
     ;;
 "here-sudo")
-    sudo $ME admin_setup $UID:$(id -g)
+    shift
+    sudo $ME admin_setup $UID:$(id -g) "$@"
     prj_build
     ;;
 "multipass")
+    shift
     multipass launch -n $JOB_NAME -c 10 -d 15G -m 16G 20.04
     multipass transfer $0 $JOB_NAME:.
-    multipass exec $JOB_NAME -- ./$MY_NAME here-sudo
+    multipass exec $JOB_NAME -- ./$MY_NAME here-sudo "$@"
     #echo "Wait for inspection"; read ignore
-    multipass transfer $JOB_NAME:$NAME.tar.gz .
+    # multipass always matches host
+    TARGET_ARCH=$(uname -m)
+    mkdir -p saved-images/host/$TARGET_ARCH
+    multipass transfer $JOB_NAME:$NAME.tar.gz saved-images/host/$TARGET_ARCH/.
     multipass delete --purge $JOB_NAME
+    ;;
+"ssh-sudo")
+    REMOTE_SSH=$2
+    shift 2
+    scp $ME $REMOTE_SSH:.
+    ssh $REMOTE_SSH ./$MY_NAME here-sudo "$@"
+    TARGET_ARCH=$(ssh $REMOTE_SSH uname -m)
+    mkdir -p saved-images/host/$TARGET_ARCH
+    scp $REMOTE_SSH:$NAME.tar.gz saved-images/host/$TARGET_ARCH/.
+    ;;
+"ec2-x86_64"|"ec2-x86")
+    shift
+    ec2 aws-$JOB_NAME run --inst m7i.2xlarge  --os-disk 15 --distro ubuntu-20.04
+    $ME ssh-sudo aws-$JOB_NAME "$@"
+    #echo "Wait for inspection"; read ignore
+    ec2 aws-$JOB_NAME destroy
+    ;;
+"ec2-arm64"|"ec2-arm")
+    shift
+    ec2 aws-$JOB_NAME run --inst m7g.2xlarge  --os-disk 15 --distro ubuntu-20.04
+    $ME ssh-sudo aws-$JOB_NAME "$@"
+    #echo "Wait for inspection"; read ignore
+    ec2 aws-$JOB_NAME destroy
     ;;
 *)
     echo "Don't understand argument $1"
